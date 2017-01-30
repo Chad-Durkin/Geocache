f097b5cf1e2d45ae77cc075e49772bd4edd9c51e
Chad Durkin and said ahmed <chad@>
2017-01-30 13:56:56 -0800

    add clickable sidebar that takes user to each page

--- js/hunt.js
+++ js/hunt.js
@@ -5,6 +5,12 @@ function Hunt(huntName, huntDifficulty, huntDescription) {
   this.huntDescription = huntDescription;
 }
 
+function clear() {
+  $("input#hunt-name").val("");
+  $("textarea#hunt-description").val("");
+  $('input[name=huntOptions]').prop('checked', false);
+}
+
 $(function() {
 var huntsArray = [];
 
@@ -16,10 +22,34 @@ var huntsArray = [];
     var theHunt = new Hunt(huntName, huntDifficulty, huntDescription);
     $("input:checkbox[name=huntOptions]:checked").each(function() {
       theHunt.huntOptions.push(this);
-    })
+
+    });
     huntsArray.push(theHunt);
-    console.log(theHunt);
-    console.log(huntsArray);
-  })
+    clear();
+    $(".create").hide();
+    $(".home").show();
+  });
+
+  $("#create-sidebar").click(function(event) {
+    event.preventDefault();
+    $(".home").hide();
+    $(".search").hide();
+    $(".create").show();
+  });
+
+  $("#search-sidebar").click(function(event) {
+    event.preventDefault();
+    $(".home").hide();
+    $(".create").hide();
+    $(".search").show();
+  });
+
+  $("#home-sidebar").click(function(event) {
+    event.preventDefault();
+    $(".create").hide();
+    $(".search").hide();
+    $(".home").show();
+  });
+
 
 });
