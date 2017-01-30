cbaeab7ec560bc74712f303bbaf3f4078de4345b
Chad Durkin and said ahmed <said@>
2017-01-30 11:55:40 -0800

    Add input vars from create hunt page

--- js/hunt.js
+++ js/hunt.js
@@ -3,3 +3,18 @@ function Hunt(huntName, huntOptions, huntDescription) {
   this.huntOptions = [huntOptions];
   this.huntDescription = huntDescription;
 }
+
+
+$(function() {
+
+  $(".createHunt").submit(function(event) {
+    event.preventDefault();
+    var huntName = $("input#hunt-name").val();
+    var huntDifficulty = $("input:radio[name=difficultyCreate]:checked").val();
+    var huntDescription = $("textarea#hunt-description").val();
+    console.log(huntName);
+    console.log(huntDifficulty);
+    console.log(huntDescription);
+  })
+
+});
