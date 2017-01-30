f1c4dc620978e857ac08125c39a776ded11f2186
Chad Durkin and said ahmed <said@>
2017-01-30 13:14:18 -0800

    add create a hunt functionality

--- js/hunt.js
+++ js/hunt.js
@@ -1,20 +1,25 @@
-function Hunt(huntName, huntOptions, huntDescription) {
+function Hunt(huntName, huntDifficulty, huntDescription) {
   this.huntName = huntName;
-  this.huntOptions = [huntOptions];
+  this.huntDifficulty = huntDifficulty;
+  this.huntOptions = [];
   this.huntDescription = huntDescription;
 }
 
-
 $(function() {
+var huntsArray = [];
 
   $(".createHunt").submit(function(event) {
     event.preventDefault();
     var huntName = $("input#hunt-name").val();
     var huntDifficulty = $("input:radio[name=difficultyCreate]:checked").val();
     var huntDescription = $("textarea#hunt-description").val();
-    console.log(huntName);
-    console.log(huntDifficulty);
-    console.log(huntDescription);
+    var theHunt = new Hunt(huntName, huntDifficulty, huntDescription);
+    $("input:checkbox[name=huntOptions]:checked").each(function() {
+      theHunt.huntOptions.push(this);
+    })
+    huntsArray.push(theHunt);
+    console.log(theHunt);
+    console.log(huntsArray);
   })
 
 });
