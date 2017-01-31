d9dfcbfeb676896f6c69954767418a9a641ee6e7
brandon cooper and sandro mijatovic <brandon@>
2017-01-31 15:19:23 -0800

    added functionality to html elements

---
+++ js/forum.js
@@ -0,0 +1,72 @@
+var outputArray = [];
+var string = "";
+
+var threadTitle = "";
+var threadCategory = "";
+var threadBody = "";
+
+function Forum(threadTitle, threadCategory, threadBody) {
+  this.threadTitle = threadTitle;
+  this.threadCategory = threadCategory;
+  this.threadBody = threadBody;
+  // this.outputArray = [];
+}
+
+Forum.prototype.printString = function() {
+  // outputArray.push(this);
+  stringTitle = "Thread title: " + this.threadTitle;
+  stringCategory = "Thread Category: " + this.threadCategory;
+  stringBody =  "Thread Body: " + this.threadBody;
+}
+
+function checker() {
+  if (threadCategory === "Gear") {
+    $('.gear').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Cataloged Hunts") {
+    $('.catalogedHunts').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Hunt Ideas") {
+    $('.huntIdeas').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Questions") {
+    $('.questions').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  $('#output').text("Your thread has been submitted. Thank You");
+
+};
+
+
+$(function () {
+  $('#thread').submit(function() {
+    event.preventDefault();
+    threadTitle = $('#threadTitle').val();
+    threadCategory = $("input:radio[name=name]:checked").val();
+    threadBody = $("#threadBody").val();
+    var forumObj = new Forum(threadTitle, threadCategory, threadBody);
+    $("input[type=radio]").prop('checked', false);
+    $("#threadTitle").val("");
+    $("#threadBody").val("");
+
+    forumObj.printString();
+
+    checker();
+
+    $('#gear1').click(function() {
+      event.preventDefault();
+      $('#gear').show();
+      });
+    $('#catalogedHunts1').click(function() {
+      event.preventDefault();
+      $('#catalogedHunts').show();
+      });
+    $('#huntIdeas1').click(function() {
+      event.preventDefault();
+      $('#huntIdeas').show();
+      });
+    $('#questions1').click(function() {
+      event.preventDefault();
+      $('#questions').show();
+      });
+});
+});
