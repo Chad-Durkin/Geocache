5c325cd4011e38e939558ee90a5e7543721b6d3e
brandon cooper and sandro mijatovic <sandro@>
2017-01-31 15:07:21 -0800

    add functionality to the html elements with js/css, allow user to create threads and search threads

--- js/forum.js
+++ js/forum.js
@@ -1,4 +1,9 @@
 var outputArray = [];
+var string = "";
+
+var threadTitle = "";
+var threadCategory = "";
+var threadBody = "";
 
 function Forum(threadTitle, threadCategory, threadBody) {
   this.threadTitle = threadTitle;
@@ -7,25 +12,60 @@ function Forum(threadTitle, threadCategory, threadBody) {
   // this.outputArray = [];
 }
 
-Forum.prototype.outputArray = function() {
-  outputArray.push(this);
+Forum.prototype.printString = function() {
+  // outputArray.push(this);
+  stringTitle = "Thread title: " + this.threadTitle;
+  stringCategory = "Thread Category: " + this.threadCategory;
+  stringBody =  "Thread Body: " + this.threadBody;
 }
+
+function goodLuck() {
+  if (threadCategory === "Gear") {
+    $('.gear').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Cataloged Hunts") {
+    $('.catalogedHunts').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Hunt Ideas") {
+    $('.huntIdeas').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  if (threadCategory === "Questions") {
+    $('.questions').append('<li>' + stringTitle + '<br/>' + stringCategory +  '<br/>' + stringBody + '</li>');
+  }
+  $('#output').text("Your thread has been submitted. Thank You");
+
+};
 
 
 $(function () {
   $('#thread').submit(function() {
     event.preventDefault();
-
-    var threadTitle = $('#threadTitle').val();
-    var threadCategory = $("input:radio[name=name]:checked").val();
-    var threadBody = $("#threadBody").val();
+    threadTitle = $('#threadTitle').val();
+    threadCategory = $("input:radio[name=name]:checked").val();
+    threadBody = $("#threadBody").val();
     var forumObj = new Forum(threadTitle, threadCategory, threadBody);
     $("input[type=radio]").prop('checked', false);
     $("#threadTitle").val("");
     $("#threadBody").val("");
-    forumObj.outputArray();
+    forumObj.printString();
 
-    console.log(outputArray);
+    goodLuck();
 
-  });
+    $('#gear1').click(function() {
+      event.preventDefault();
+      $('#gear').show();
+      });
+    $('#catalogedHunts1').click(function() {
+      event.preventDefault();
+      $('#catalogedHunts').show();
+      });
+    $('#huntIdeas1').click(function() {
+      event.preventDefault();
+      $('#huntIdeas').show();
+      });
+    $('#questions1').click(function() {
+      event.preventDefault();
+      $('#questions').show();
+      });
+});
 });
