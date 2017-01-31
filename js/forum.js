77a75fe2aa2e0fbb9bedd973f387f9bee9e0befd
brandon cooper and sandro mijatovic <brandon@>
2017-01-31 11:53:27 -0800

    add reset form on submit, store submit in global array

--- js/forum.js
+++ js/forum.js
@@ -4,26 +4,28 @@ function Forum(threadTitle, threadCategory, threadBody) {
   this.threadTitle = threadTitle;
   this.threadCategory = threadCategory;
   this.threadBody = threadBody;
-  this.outputArray = [];
+  // this.outputArray = [];
 }
 
 Forum.prototype.outputArray = function() {
-  this.outputArray.push(this);
+  outputArray.push(this);
 }
 
 
 $(function () {
   $('#thread').submit(function() {
     event.preventDefault();
+
     var threadTitle = $('#threadTitle').val();
     var threadCategory = $("input:radio[name=name]:checked").val();
     var threadBody = $("#threadBody").val();
-
     var forumObj = new Forum(threadTitle, threadCategory, threadBody);
-
+    $("input[type=radio]").prop('checked', false);
+    $("#threadTitle").val("");
+    $("#threadBody").val("");
     forumObj.outputArray();
 
-    console.log(forumObj.outputArray);
+    console.log(outputArray);
 
   });
 });
