39410bee5b5c390ec7ae450b64eaccd9f40b66c7
Chad Durkin and Said Ahmed <said@>
2017-02-01 10:37:07 -0800

    Added hunt functionality for showing hunt pages

--- js/hunt.js
+++ js/hunt.js
@@ -1,4 +1,4 @@
-function Hunt(huntName, huntDifficulty, huntDescription, hunterCounter) {
+function Hunt(huntName, huntDifficulty, huntDescription, huntCounter) {
   this.huntName = huntName;
   this.huntDifficulty = huntDifficulty;
   this.huntOptions = [];
@@ -13,16 +13,37 @@ Hunt.prototype = {
     for(var index = 0; index < searchArray.length; index++)
     {
       for(var i = 0; i < this.huntOptions.length; i++) {
-        if(this.huntOptions[i] === searchArray[index]) {
+        if(this.huntOptions[i] === searchArray[index] || this.huntDifficulty === searchArray[index]) {
           $("#matched-hunts").append("<li class='listedHunts" + listCounter + "'> <strong>Hunt Name:</strong> <em>" + this.huntName + "</em> <strong>Difficulty:</strong> <em>" + this.huntDifficulty + "</em></li>");
           $(".listedHunts" + listCounter).append("<p> <strong>Description:</strong> <em>" + this.huntDescription + "</em></p>");
           listCounter++;
+          return;
         }
       }
     }
   }
 };
 
+function clickHunt(huntsArray) {
+  $(".currentHunts").last().click(function(event) {
+    event.preventDefault();
+    $(".showHuntGearRequired").empty();
+    $(".home").hide();
+    $(".search").hide();
+    $(".create").hide();
+    for (var index = 0; index < huntsArray.length; index++) {
+      if(huntsArray[index].huntCounter === this.id) {
+        $(".showHuntHeader").text(huntsArray[index].huntName);
+        $(".showHuntDifficulty").text("Difficulty: " + huntsArray[index].huntDifficulty);
+        huntsArray[index].huntOptions.forEach(function(option) {
+          $(".showHuntGearRequired").append("<li>" + option + "</li>");
+        })
+        $(".showHuntDescription").text(huntsArray[index].huntDescription);
+      }
+    }
+    $(".showHunt").show();
+  });
+}
 function clear() {
   $("input#hunt-name").val("");
   $("textarea#hunt-description").val("");
@@ -32,6 +53,8 @@ function clear() {
 $(function() {
 var huntsArray = [];
 var listCounter = 1;
+var demoHunt = new Hunt("Demo Hunt! Check out the teams hunt for you!", "Journeyman", "This is our demo hunt! We will fill in a more appropriate description for it later, but right now we are giving you, the user, an idea of what you will be able to see on this site and use on this site, but for now this will be an overview or a basic description of this hunt, some expectations, some advice, what you will be seeing or doing on this hunt, and anything else the hunt create feels useful for the people who wants to choose to do his hunt.", "hunt0");
+huntsArray.push(demoHunt);
 
   $(".createHunt").submit(function(event) {
     event.preventDefault();
@@ -39,6 +62,7 @@ var listCounter = 1;
     var huntDifficulty = $("input:radio[name=difficultyCreate]:checked").val();
     var huntDescription = $("textarea#hunt-description").val();
     var huntCounter = "hunt" + listCounter;
+    console.log(huntCounter);
     var theHunt = new Hunt(huntName, huntDifficulty, huntDescription, huntCounter);
     $("input:checkbox[name=huntOptions]:checked").each(function() {
       theHunt.huntOptions.push($(this).val());
@@ -51,20 +75,10 @@ var listCounter = 1;
     $("#current-hunts").append("<li> <strong>Hunt Name:</strong> <em><span class='currentHunts' id='hunt" + listCounter + "'>" + theHunt.huntName + "</span></em> <strong>Difficulty:</strong> <em>" + theHunt.huntDifficulty + "</em></li>");
     $(".currentHuntList" + listCounter).append("<p> <strong>Description:</strong> <em>" + theHunt.huntDescription + "</em></p>");
     listCounter++;
-    // $(".currentHunts").click(function(event) {
-    //   event.preventDefault();
-    //   $(".home").hide();
-    //   $(".search").hide();
-    //   $(".create").hide();
-    // });
+    clickHunt(huntsArray);
   });
 
-  // $(".currentHunts").click(function(event) {
-  //   event.preventDefault();
-  //   $(".home").hide();
-  //   $(".search").hide();
-  //   $(".create").hide();
-  // });
+  clickHunt(huntsArray);
 
 $(".searchHunt").submit(function(event) {
     event.preventDefault();
@@ -82,6 +96,7 @@ $(".searchHunt").submit(function(event) {
     event.preventDefault();
     clear();
     $("#matched-hunts").empty();
+    $(".showHunt").hide();
     $(".home").hide();
     $(".search").hide();
     $(".create").show();
@@ -91,6 +106,7 @@ $(".searchHunt").submit(function(event) {
     event.preventDefault();
     clear();
     $("#matched-hunts").empty();
+    $(".showHunt").hide();
     $(".home").hide();
     $(".create").hide();
     $(".search").show();
@@ -100,6 +116,7 @@ $(".searchHunt").submit(function(event) {
     event.preventDefault();
     clear();
     $("#matched-hunts").empty();
+    $(".showHunt").hide();
     $(".create").hide();
     $(".search").hide();
     $(".home").show();
