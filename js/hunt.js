31f2ca679be75a9328829e9118e569075bc5180d
Chad Durkin and Said Ahmed <said@>
2017-02-01 14:46:54 -0800

    add coordinate functionality

--- js/hunt.js
+++ js/hunt.js
@@ -4,6 +4,9 @@ function Hunt(huntName, huntDifficulty, huntDescription, huntCounter) {
   this.huntOptions = [];
   this.huntDescription = huntDescription;
   this.huntCounter = huntCounter;
+  this.huntSteps = [];
+  this.huntStepsCoords = [];
+  this.huntCoordDirection = [];
 };
 
 //Added in comment for commit sake
@@ -12,7 +15,7 @@ Hunt.prototype = {
     var listCounter = 0;
     for(var index = 0; index < searchArray.length; index++)
     {
-      for(var i = 0; i < this.huntOptions.length; i++) {
+      for(var i = 0; i <= this.huntOptions.length; i++) {
         if(this.huntOptions[i] === searchArray[index] || this.huntDifficulty === searchArray[index]) {
           $("#matched-hunts").append("<li class='listedHunts" + listCounter + "'> <strong>Hunt Name:</strong> <em>" + this.huntName + "</em> <strong>Difficulty:</strong> <em>" + this.huntDifficulty + "</em></li>");
           $(".listedHunts" + listCounter).append("<p> <strong>Description:</strong> <em>" + this.huntDescription + "</em></p>");
@@ -38,7 +41,13 @@ function clickHunt(huntsArray) {
         huntsArray[index].huntOptions.forEach(function(option) {
           $(".showHuntGearRequired").append("<li>" + option + "</li>");
         })
+        console.log("here1");
         $(".showHuntDescription").text(huntsArray[index].huntDescription);
+        for(var index2 = 0; index2 < huntsArray[index].huntSteps.length; index2++) {
+          console.log("here2");
+          var stepCoord = "Latitude: " + huntsArray[index].huntCoordDirection[0] + " " + huntsArray[index].huntStepsCoords[index2][0] + "." + huntsArray[index].huntStepsCoords[index2][1] + "˚ Longitude: " + huntsArray[index].huntCoordDirection[1] + " " + huntsArray[index].huntStepsCoords[index2][2] + "." + huntsArray[index].huntStepsCoords[index2][3] + "˚";
+          
+        }
       }
     }
     $(".showHunt").show();
@@ -47,21 +56,29 @@ function clickHunt(huntsArray) {
 
 function addStep(stepCounter) {
   console.log("here");
-  $("#step-div").append("<div class='row'><br><div class='col-md-12'><div class='form-group'><h4>Step " + stepCounter + ": </h4><input type='text' class='form-control steps' name='steps' id='step" + stepCounter + "' required></div></div></div><div class='row'><div class='col-md-4'><div class='form-group'><h4>Latitude: </h4><select class='form-control compass'><option>N</option><option>S</option></select><input type='number' class='form-control degreesPre coords' name='coords' required><p class='coordPeriod'>.</p><input type='number' class='form-control degreesPost coords' name='coords' required><p class='coordDegree'>˚</p></div></div><div class='col-md-4'><div class='form-group'><h4>Longitude: </h4><select class='form-control compass'><option>E</option><option>W</option></select><input type='number' class='form-control step1Coords degreesPre coords' name='coords' required><p class='coordPeriod'>.</p><input type='number' class='form-control step1Coords degreesPost coords' name='coords' required><p class='coordDegree'>˚</p></div></div></div>");
+  $("#step-div").append("<div class='step" + stepCounter + "'><div class='row'><br><div class='col-md-12'><div class='form-group'><h4>Step " + stepCounter + ": </h4><input type='text' class='form-control steps' name='steps' id='step" + stepCounter + "' required></div></div></div><div class='row'><div class='col-md-4'><div class='form-group'><h4>Latitude: </h4><select class='form-control compass'><option>N</option><option>S</option></select><input type='number' class='form-control degreesPre coords' name='coord" + stepCounter + "' required><p class='coordPeriod'>.</p><input type='number' class='form-control degreesPost coords' name='coord" + stepCounter + "' required><p class='coordDegree'>˚</p></div></div><div class='col-md-4'><div class='form-group'><h4>Longitude: </h4><select class='form-control compass'><option>E</option><option>W</option></select><input type='number' class='form-control step1Coords degreesPre coords' name='coord" + stepCounter + "' required><p class='coordPeriod'>.</p><input type='number' class='form-control step1Coords degreesPost coords' name='coord" + stepCounter + "' required><p class='coordDegree'>˚</p></div></div></div></div>");
   return stepCounter += 1;
 };
 
-function clear() {
+function clear(stepCounter) {
   $("input#hunt-name").val("");
   $("textarea#hunt-description").val("");
   $("input[name=huntOptions]").prop("checked", false);
+  $("input#step1").val("");
+  $("input[name=coords]").val("");
+  for(var index = stepCounter - 1; index > 1; index--) {
+    $(".step" + index).remove();
+  }
+  stepCounter = 2;
+  return stepCounter;
 };
 
 $(function() {
 var huntsArray = [];
 var listCounter = 1;
 var stepCounter = 2;
-var demoHunt = new Hunt("Demo Hunt! Check out the teams hunt for you!", "Journeyman", "This is our demo hunt! We will fill in a more appropriate description for it later, but right now we are giving you, the user, an idea of what you will be able to see on this site and use on this site, but for now this will be an overview or a basic description of this hunt, some expectations, some advice, what you will be seeing or doing on this hunt, and anything else the hunt create feels useful for the people who wants to choose to do his hunt.", "hunt0");
+var eachStep = 1;
+var demoHunt = new Hunt("Demo Hunt! Check out the teams hunt for you!", "journeyman", "This is our demo hunt! We will fill in a more appropriate description for it later, but right now we are giving you, the user, an idea of what you will be able to see on this site and use on this site, but for now this will be an overview or a basic description of this hunt, some expectations, some advice, what you will be seeing or doing on this hunt, and anything else the hunt create feels useful for the people who wants to choose to do his hunt.", "hunt0");
 huntsArray.push(demoHunt);
 
   $(".createHunt").submit(function(event) {
@@ -70,13 +87,27 @@ huntsArray.push(demoHunt);
     var huntDifficulty = $("input:radio[name=difficultyCreate]:checked").val();
     var huntDescription = $("textarea#hunt-description").val();
     var huntCounter = "hunt" + listCounter;
-    console.log(huntCounter);
+    var huntStepCounter = 0;
     var theHunt = new Hunt(huntName, huntDifficulty, huntDescription, huntCounter);
     $("input:checkbox[name=huntOptions]:checked").each(function() {
       theHunt.huntOptions.push($(this).val());
     });
+    theHunt.huntCoordDirection.push($("#latCompass").val());
+    theHunt.huntCoordDirection.push($("#longCompass").val());
+    var coordArray = [];
+    $("input[name=steps]").each(function() {
+      theHunt.huntSteps.push($(this).val());
+      coordArray = [];
+
+      $("input[name=coord" + eachStep + "]").each(function() {
+        coordArray.push($(this).val());
+      })
+      eachStep++;
+      theHunt.huntStepsCoords.push(coordArray);
+    })
     huntsArray.push(theHunt);
-    clear();
+    stepCounter = clear(stepCounter);
+    stepCounter = 2;
     $(".create").hide();
     $(".home").show();
 
@@ -89,7 +120,7 @@ huntsArray.push(demoHunt);
   $("#add-step").click(function(event) {
     event.preventDefault();
     stepCounter = addStep(stepCounter);
-  })
+  });
 
   clickHunt(huntsArray);
 
@@ -107,7 +138,7 @@ $(".searchHunt").submit(function(event) {
 
   $("#create-sidebar").click(function(event) {
     event.preventDefault();
-    clear();
+    stepCounter = clear(stepCounter);
     $("#matched-hunts").empty();
     $(".showHunt").hide();
     $(".home").hide();
@@ -117,7 +148,7 @@ $(".searchHunt").submit(function(event) {
 
   $("#search-sidebar").click(function(event) {
     event.preventDefault();
-    clear();
+    stepCounter = clear(stepCounter);
     $("#matched-hunts").empty();
     $(".showHunt").hide();
     $(".home").hide();
@@ -127,7 +158,7 @@ $(".searchHunt").submit(function(event) {
 
   $("#home-sidebar").click(function(event) {
     event.preventDefault();
-    clear();
+    stepCounter = clear(stepCounter);
     $("#matched-hunts").empty();
     $(".showHunt").hide();
     $(".create").hide();
